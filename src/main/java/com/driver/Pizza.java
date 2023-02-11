45587bf9ac6c75df626a305ef6207e81805c0666
unknown <verma.shubham2537@gmail.com>
2023-02-11 08:27:50 +0530

    Pehala commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,24 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private int toppings;
+
+    boolean isaddExtraCheese;
+    boolean isaddExtraToppings;
+    boolean isBillCreated;
+    boolean isTakeaway;
+
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        this.price = isVeg ? 300 : 400;
+        this.toppings = isVeg ? 70 : 120;
+        this.isaddExtraCheese = false;
+        this.isaddExtraToppings = false;
+        this.isTakeaway = false;
+        this.isBillCreated = false;
+        this.bill = "Base Price Of The Pizza: " + this.price + "\n";
     }
 
     public int getPrice(){
@@ -16,19 +30,40 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(!isaddExtraCheese){
+            this.price += 80;
+            this.isaddExtraCheese = true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!isaddExtraToppings){
+            this.price += this.toppings;
+            isaddExtraToppings = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!isTakeaway){
+            this.price += 20;
+            isTakeaway = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        if(!isBillCreated) {
+            if (isaddExtraCheese)
+                this.bill += "Extra Cheese Added: 80" + "\n";
+            if (isaddExtraToppings)
+                this.bill += "Extra Toppings Added: " + this.toppings + "\n";
+            if (isTakeaway)
+                this.bill += "Paperbag Added: 20" + "\n";
+            this.bill += "Total Price: " + this.price;
+            this.isBillCreated = true;
+        }
         return this.bill;
     }
 }
